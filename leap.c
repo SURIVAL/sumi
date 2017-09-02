91df2c364f28bb5c89b2b6b9eab9956d4c28e63c
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-02 19:52:37 +0530

    leap.c

---
+++ leap.c
@@ -0,0 +1,17 @@
+# sumi
+#include<stdio.h>ear);
+void main()
+{
+int year;
+printf("enter the leapyear");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("%d is leapyear", year);
+}
+else
+{
+printf("%dis notaleapyear",year);
+}
+getch();
+}
