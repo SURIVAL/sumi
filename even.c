e0e136e5ebfe78ed39e74a645ca5f1acf5bf8bce
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 09:55:51 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,17 @@
+# sumi
+#include<stdio.h>ear);
+void main()
+{
+int n;
+printf("enter the n values");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d is even");
+}
+else
+{
+printf("%dis odd");
+}
+getch();
+}
